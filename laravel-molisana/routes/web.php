e3fbc5aa6391ebc5999085dd1631f0bf4e915dcd
Michele Cofano <brutalmachinelol@gmail.com>
2021-03-11 20:40:42 +0100

    added product page (with product specs)

--- laravel-molisana/routes/web.php
+++ laravel-molisana/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products/n-5', function () {
+    return view('products.n-5');
+});
